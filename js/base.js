dbd19ab6ade67f4f3fe98818f53cc433dce63eb6
itakurakoichi <itakurakoichi.w@gmail.com>
2016-01-15 11:38:10 +0900

    hello world

---
+++ js/base.js
@@ -0,0 +1,12 @@
+(function(w) {
+	'use strict';
+
+	w.document.write('hi.');
+
+	var helloWorld = function() {
+		console.log('Hello Hello Hello Hello Hello.');
+	};
+	helloWorld();
+
+	
+})(window);
